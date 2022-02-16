61ba9cd2ef92fd274e7621e69ee4e2bde8ea672a
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-02-15 20:46:44 -0800

    Update index.php

--- index.php
+++ index.php
@@ -69,6 +69,22 @@
       </div>
       <hr>
     </div>
+      
+            <div id="skills" class="content-item">
+        <div class="vertical-center">
+          <h2>Skills</h2>
+          <p>...</p>
+        </div>
+      </div>
+      <hr>
+      
+           <div id="Interests" class="content-item">
+        <div class="vertical-center">
+          <h2>Interests</h2>
+          <p>...</p>
+        </div>
+      </div>
+      <hr>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
