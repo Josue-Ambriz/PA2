a6de35f4eb2a7a3c1eab30865c563c4001767012
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-02-15 20:51:23 -0800

    Update index.php

--- index.php
+++ index.php
@@ -69,23 +69,28 @@
       </div>
       <hr>
     </div>
-      
-            <div id="skills" class="content-item">
+
+    <div id="skills" class="content-item">
         <div class="vertical-center">
           <h2>Skills</h2>
-          <p>...</p>
+          <?php foreach($json_data['skills'] AS $skills) { ?>
+              <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                  <h3 class="mb-0"><?php echo $skills['title']; ?></h3>
+                  <div class="subheading mb-3"><?php echo $skills['employer']; ?></div>
+                  <p><?php echo $eskills['description']; ?></p>
+                </div>
+                <div class="flex-shrink-0">
+                  <span class="text-primary"><?php echo $skills['period']; ?></span>
+                </div>
+              </div>
+          <?php  } ?>
         </div>
       </div>
       <hr>
+    </div>
       
-           <div id="Interests" class="content-item">
-        <div class="vertical-center">
-          <h2>Interests</h2>
-          <p>...</p>
-        </div>
-      </div>
-      <hr>
-
+      
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
